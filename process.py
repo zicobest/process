a7d0893025efe5d82c7cecd96e503321d57f3639
isaacbest <zicolas4ever@yahoo.com>
2023-09-08 23:21:25 +0200

    my process

--- process.py
+++ process.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+
+def import_csv(file_path):
+    event_log = pd.read_csv(file_path, sep=',')
+    num_events = len(event_log)
+    num_case = len(event_log.case_id.unique())  # to avoid repetition
+    print("Number of events{}\n number of cases:{}".format(num_events, num_case))
+
+
+if __name__ == "__main__":
+    import_csv("p2p_event_log.csv")
+    print(pd.read_csv("p2p_event_log.csv"))
